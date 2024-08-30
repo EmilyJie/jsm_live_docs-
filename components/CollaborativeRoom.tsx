2e0397a7c660f0c6e49f08ba2ac6cecb4e848274
Yi-Jhen Jie <emily6331@gmail.com>
2024-08-31 00:21:48 +0800

    feat: Add getClerkUsers function to fetch user data for ActiveCollaborators component

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -4,6 +4,7 @@ import { ClientSideSuspense, RoomProvider } from "@liveblocks/react/suspense";
 import { Editor } from "@/components/editor/Editor";
 import Header from "@/components/Header";
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import ActiveCollaborators from "./ActiveCollaborators";
 
 export default function CollaborativeRoom() {
     return (
@@ -14,12 +15,16 @@ export default function CollaborativeRoom() {
                         <div className="flex w-fit items-center justify-center gap-2">
                             <p className="document-title">Share</p>
                         </div>
-                        <SignedOut>
-                            <SignInButton />
-                        </SignedOut>
-                        <SignedIn>
-                            <UserButton />
-                        </SignedIn>
+
+                        <div className="flex w-full flex-1 justify-end gap-2 sm:gap-3">
+                            <ActiveCollaborators />
+                            <SignedOut>
+                                <SignInButton />
+                            </SignedOut>
+                            <SignedIn>
+                                <UserButton />
+                            </SignedIn>
+                        </div>
                     </Header>
                     <Editor />
                 </div>
